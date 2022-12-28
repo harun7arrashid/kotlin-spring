8008c459706864dfe2ec7ac45adbbc288ef9f16d
Harun AR-Rashid <harun4arrashid@gmail.com>
2022-12-28 14:08:13 +0700

    First commit

---
+++ src/main/kotlin/id/ackerman/restful/KotlinResTfulApiApplication.kt
@@ -0,0 +1,11 @@
+package id.ackerman.restful
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinResTfulApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinResTfulApiApplication>(*args)
+}
---
+++ src/test/kotlin/id/ackerman/restful/KotlinResTfulApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package id.ackerman.restful
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinResTfulApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
